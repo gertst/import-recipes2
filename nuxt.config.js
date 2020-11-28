49b8afc17c4dc1a218eea7c4cca2acda85a7585d
Gert Stalpaert <gertst@gmail.com>
2020-11-28 22:42:02 +0100

    SSR

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ let dynamicRoutes = () => {
 export default {
 
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: false,
+  ssr: true,
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
